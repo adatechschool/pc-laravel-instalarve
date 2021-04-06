b9aefaa538ca52e82a30cab3aacce78ade194cd1
Thomas 'tob' Blochet <tbsx@outlook.fr>
2021-04-06 16:09:02 +0200

    [refacto+add][welcome] changer boutons + message

--- instalarve-app/resources/views/welcome.blade.php
+++ instalarve-app/resources/views/welcome.blade.php
@@ -32,25 +32,25 @@
                     <p>Un super r&eacute;seau social trop bien !</p>
                 </div>
                 <div>
-                    @if (Route::has('login'))
                     <div class="px-6 py-4 sm:block">
-                        @else
-                        <a href="{{route('login')}}" class="bg-blue-500 px-2 py-1
-                                text-white font-semibold text-sm rounded block text-center
-                                sm:inline-block block">Log In</a>
-
-                        @if (Route::has('register'))
-                        <a href="{{route('register')}}" class="bg-gray-300 px-2 py-1 border
-                                    text-white font-semibold text-sm rounded block text-center
-                                    sm:inline-block block">Register</a>
-                        @endif
-                        @endauth
+                        <a href="{{route('login')}}" ><x-button class="bg-blue-500 px-2 py-1
+                            text-white font-semibold text-sm rounded block text-center
+                            sm:inline-block block">{{ __('Log In') }}</x-button></a>
+                        <a href="{{route('register')}}" ><x-button class=" px-2 py-1 
+                            text-white font-semibold text-sm rounded block text-center
+                            sm:inline-block block">
+                                        {{ __('Register') }}
+                                    </x-button></a>
                     </div>
-                    @endif
                 </div>
             </div>
+            <footer>
+                Fait avec <i class="text-red-500 fas fa-heart"></i> par <a href="https://github.com/Nils92" class="font-bold text-red-500">Nils</a>, <a href="https://github.com/aliceKatkout" class="font-bold text-yellow-500">Alice</a>, <a href="https://github.com/Francel1n" class="font-bold text-green-500">Francelin </a>, <a href="https://github.com/QuentinBerard" class="font-bold text-blue-500">Quentin</a> & <a href="https://github.com/tob-0" class="font-bold text-indigo-500">Thomas</a>.
+            </footer>
         </div>
+        
     </article>
 </body>
 
 </html>
+
